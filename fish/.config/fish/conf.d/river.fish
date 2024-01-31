ac61138bf45da103892d98d86d05280f43ba3d2e
Allison Durham <github@allisons.email>
2024-01-31 11:09:25 -0600

    Go back

--- fish/.config/fish/conf.d/river.fish
+++ fish/.config/fish/conf.d/river.fish
@@ -1,3 +1,4 @@
 # https://github.com/flameshot-org/flameshot/blob/master/docs/Sway%20and%20wlroots%20support.md#river-wlroots-support
 # Have to set CURRENT_DESKTOP to sway to trick flameshot into working. launch river like: dbus-run-session river
-set -gx XDG_CURRENT_DESKTOP sway
+# I may go away from flameshot, setting back to river and launching river normally.
+set -gx XDG_CURRENT_DESKTOP river
