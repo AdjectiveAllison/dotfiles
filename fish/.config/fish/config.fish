f5290d32233616bdff4ce1a43db4a607f7ba0b5b
Allison Durham <github@allisons.email>
2024-01-31 11:57:53 -0600

    Some small updates

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,6 +1,10 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
-    atuin init fish | source
+
+    # no atuin temporarily. I like native fish up arrow for history more. 
+    # TODO: Figure out if it's useful to have activated for storing commands in a way that isn't just up arrow history.
+    #atuin init fish | source
+    
     starship init fish | source
 end
 
