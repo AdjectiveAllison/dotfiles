2e6c6cc93029de7e36ca3b80b1e9faa7763f3057
Allison Durham <github@allisons.email>
2024-02-02 12:08:25 -0600

    throw this nonsense into git before I trash it so it's in history.

---
+++ terminal_general/fish/.config/fish/functions/gruvbox_material.fish
@@ -0,0 +1,36 @@
+#All colors manually fetched from gruvbox-material-dark vscode extension.
+
+#this is a guess
+set -U fish_color_normal D4BE98
+
+#pretty confident on this one
+set -U fish_color_command 7DAEA3
+
+# feeling good about it.
+set -U fish_color_quote D8A657
+
+# TODO: What is redirection, what color do we use.
+#set fish_color_redirection NO IDEA
+
+#meh this one is okay
+set -U fish_color_end E78A4E
+
+# Confident on this one.
+set -U fish_color_error EA6962
+
+#setting to same as normal, we'll see how it looks.
+set -U fish_color_param D4BE98
+
+#Same as vscode comment.
+set -U fish_color_comment 928374
+
+
+# TODO: No idea what match is:
+#set fish_color_match ??
+#set to bright red temporarily
+#set fish_color_match FF3333
+
+
+#these should both be decent but idk what they even do honestly.
+set -U fish_color_selection --background=A89984
+set -U fish_color_search_match --background=A89984
