173088721c77bb3ffd7107d985ed3cc572717706
Allison Durham <github@allisons.email>
2024-02-02 09:35:03 -0600

    nice little alias

--- terminal_general/fish/.config/fish/config.fish
+++ terminal_general/fish/.config/fish/config.fish
@@ -1,6 +1,9 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
 
+    # Helps screen run better when sshing from graphical terminal not on server
+    alias ssh 'TERM=xterm-256color /usr/bin/ssh'
+
     # no atuin temporarily. I like native fish up arrow for history more. 
     # TODO: Figure out if it's useful to have activated for storing commands in a way that isn't just up arrow history.
     #atuin init fish | source
