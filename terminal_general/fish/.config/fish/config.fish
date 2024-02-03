6ed33e7356050e34e11d597d53dc88bee4d77ae6
Allison Durham <github@allisons.email>
2024-02-03 09:31:34 -0600

    no longer alias ssh

--- terminal_general/fish/.config/fish/config.fish
+++ terminal_general/fish/.config/fish/config.fish
@@ -2,7 +2,8 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 
     # Helps screen run better when sshing from graphical terminal not on server
-    alias ssh 'TERM=xterm-256color /usr/bin/ssh'
+    #alias ssh 'TERM=xterm-256color /usr/bin/ssh'
+    #use this to send terminfo to other server to make the alias not needed: infocmp -x | ssh HOSTNAME -- tic -x -
 
     # no atuin temporarily. I like native fish up arrow for history more. 
     # TODO: Figure out if it's useful to have activated for storing commands in a way that isn't just up arrow history.
