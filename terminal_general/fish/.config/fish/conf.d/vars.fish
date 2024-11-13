a4329cc2cb84d022ab8badcfe59b25c267327ac6
Allison Durham <a@AdjectiveAllison.com>
2024-11-13 09:55:20 -0600

    using ~/.local/bin for PATH in fish I guess

--- terminal_general/fish/.config/fish/conf.d/vars.fish
+++ terminal_general/fish/.config/fish/conf.d/vars.fish
@@ -1,7 +1,6 @@
 # list of public git repo vars
 set -gx EDITOR nvim
-
-
+set -gx PATH $HOME/.local/bin $PATH
 
 # function call to source ~/.global.env and set -gx on them (SECRETS NOT FOR GIT)
 [ -e ~/.global.env ]; and source_env ~/.global.env
